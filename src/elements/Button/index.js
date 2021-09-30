953ff5f9c29d3aa804fc86322a5ce5d4d90685d4
solihahnur <57170706+gryto@users.noreply.github.com>
2021-09-30 15:22:41 +0700

    create Header and Landingpage

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import propTypes from 'prop-types'
 
 export default function Button(props) {
